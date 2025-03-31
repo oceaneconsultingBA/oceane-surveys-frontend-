f4ce5caef52e89f7bd91eafc12e8ba640bdb3864
Youcef GUERAINI (EXT) <youcef.gueraini.ext@up.coop>
2025-03-31 13:38:33 +0200

    Add Survey

--- src/app/components/survey/survey.component.ts
+++ src/app/components/survey/survey.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { StepsModule } from 'primeng/steps';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { QuestionComponent } from '../question/question.component'; 
 
 @Component({
   selector: 'app-survey',
-  imports: [],
+  imports: [StepsModule, ButtonModule, ReactiveFormsModule, TableModule, CommonModule, QuestionComponent],
   templateUrl: './survey.component.html',
   styleUrl: './survey.component.scss'
 })
 export class SurveyComponent {
+  activeStep = 0;
+  surveyForm: FormGroup;
+  questions: any[] = [];
 
+  steps = [
+    { label: 'Informations' },
+    { label: 'Questions' },
+    { label: 'Validation' }
+  ];
+
+  constructor(private fb: FormBuilder) {
+    this.surveyForm = this.fb.group({
+      surveyName: ['', Validators.required]
+    });
+  }
+
+  nextStep() {
+    if (this.activeStep < this.steps.length - 1) {
+      this.activeStep++;
+    }
+  }
+
+  prevStep() {
+    if (this.activeStep > 0) {
+      this.activeStep--;
+    }
+  }
+
+  removeQuestion(question: any) {
+    this.questions = this.questions.filter(q => q !== question);
+  }
 }
