1052712e37e334c054a48d8c8d1c992bd637ffd0
Youcef GUERAINI (EXT) <youcef.gueraini.ext@up.coop>
2025-03-31 13:38:33 +0200

    Add Survey

--- src/app/components/survey/survey.component.ts
+++ src/app/components/survey/survey.component.ts
@@ -1,112 +1,63 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { Survey } from '../../models/survey';
-import { FormBuilder, Validators } from '@angular/forms';
-import { MessageService } from 'primeng/api';
-import { Recipient } from '../../models/recipient';
-import { RecipientType } from '../../models/recipient-type';
-import { RecipientService } from '../../services/recipient-service.service';
-import { SurveyStatus } from '../../models/survey-status';
-import { Question } from '../../models/question';
-import { SurveyService } from '../../services/survey-service.service';
-import { Table, TableModule } from 'primeng/table';
-import { ButtonModule } from 'primeng/button';
-import { ToastModule } from 'primeng/toast';
-import { ToolbarModule } from 'primeng/toolbar';
-import { InputTextModule } from 'primeng/inputtext';
-import { TextareaModule } from 'primeng/textarea';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SelectModule } from 'primeng/select';
-import { IconFieldModule } from 'primeng/iconfield';
-import { InputIconModule } from 'primeng/inputicon';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { StepsModule } from 'primeng/steps';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { DropdownModule } from 'primeng/dropdown';
+import { QuestionComponent } from '../question/question.component';
+import { RecipientComponent } from "../recipient/recipient.component"; 
 
 @Component({
   selector: 'app-survey',
-  imports: [
-    TableModule,
-    ButtonModule,
-    SelectModule,
-    ToastModule,
-    ToolbarModule,
-    InputTextModule,
-    TextareaModule,
-    CommonModule,
-        IconFieldModule,
-        InputIconModule
-  ],
+  imports: [StepsModule, ButtonModule, ReactiveFormsModule, TableModule, CommonModule,
+     QuestionComponent, RecipientComponent, DropdownModule],
   templateUrl: './survey.component.html',
   styleUrl: './survey.component.scss'
 })
-export class SurveyComponent implements OnInit {
-  enquetes!: Survey[];
-  enquete!: Survey;
-  submitted: boolean = false;
-  statuses!: any[];
-  modalMode!: String;
-  SurveyStatus = SurveyStatus;
-  
-  @ViewChild('dt') dt!: Table;
+export class SurveyComponent {
+  activeStep = 0;
+  surveyForm: FormGroup;
+  questions: any[] = [];
 
-  constructor(
-    private messageService: MessageService,
-    private surveyService: SurveyService
-  ) {
+  description: string = '';
+  enqueteTypes = [
+    { label: 'Satisfaction Consultant', value: 'satisfaction_consultant' },
+    { label: 'Satisfaction Client', value: 'satisfaction_client' },
+    { label: 'Autre', value: 'autre' }
+  ];
+  frequencies = [
+    { label: 'Mensuelle', value: 'mensuelle' },
+    { label: 'Trimestrielle', value: 'trimestrielle' },
+    { label: 'Annuelle', value: 'annuelle' }
+  ];
 
-  }
+  steps = [
+    { label: 'Informations' },
+    { label: 'Questions' },
+    { label: 'Destinataires' },
+    { label: 'Validation' }
+  ];
 
-  ngOnInit(): void {
-    const questions: Question[] = []; 
-    this.getSurveys();
-  }
-
-  private getSurveys() {
-    this.surveyService.getSurveys().subscribe({
-      next: (data: any) => {
-        console.log('Enquêtes récupérées avec succès :', data);
-        this.enquetes = data; // Met à jour la liste des enquêtes
-      },
-      error: (err: any) => {
-        console.error('Erreur lors du chargement des enquêtes :', err);
-        this.messageService.add({ severity: 'error', summary: 'Erreur', detail: 'Échec du chargement des enquêtes' });
-      }
-    });
-  }
-  
-
-
-  // Modifier un recipient existant
-  editeEnquete(enquete: Survey) {
-
-  }
-
-  // Ouvrir la fenêtre de création
-  openNew() {
-    //this.recepientDialog = true;
-    this.submitted = false;
-    this.modalMode = "CREATE";
-    //this.recipientForm.reset();
-  }
-
-
-
-  // Supprimer un recipient
-  deleteEnquete(enquete: Survey) {
-    this.surveyService.deleteSurvey(enquete.id).subscribe({
-      next: (data: any) => {
-        console.log('Enquete supprimé avec succès :', data);
-        this.enquetes = this.enquetes.filter(r => r.id !== enquete.id);
-        this.messageService.add({ severity: 'success', summary: 'Succès', detail: 'Enquete supprimé avec succès' });
-      },
-      error: (err: any) => {
-        console.error('Erreur lors de la suppression  :', err);
-        this.messageService.add({ severity: 'error', summary: 'Erreur', detail: 'Échec de la suppression' });
-      }
+  constructor(private fb: FormBuilder) {
+    this.surveyForm = this.fb.group({
+      surveyName: ['', Validators.required]
     });
   }
 
-
-  globalSearch(event: Event):void{
-    const target = (event.target as HTMLInputElement);
-    this.dt.filterGlobal(target.value, 'contains');
+  nextStep() {
+    if (this.activeStep < this.steps.length - 1) {
+      this.activeStep++;
+    }
   }
 
+  prevStep() {
+    if (this.activeStep > 0) {
+      this.activeStep--;
+    }
+  }
+
+  removeQuestion(question: any) {
+    this.questions = this.questions.filter(q => q !== question);
+  }
 }
